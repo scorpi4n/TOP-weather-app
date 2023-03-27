bf2b55fa28e99693a4809078d06a8d8973a4fc35
James Phillips <scorpi4n@scorpi4n.com>
2023-03-27 13:46:00 -0500

    Create interface for API response for intellisense

--- src/ts/weather.ts
+++ src/ts/weather.ts
@@ -23,3 +23,46 @@ export async function getForecast(
 
   return await res.json();
 }
+
+export interface CurrentWeatherRes {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+    sea_level: number;
+    grnd_level: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
