1bfa536f9832c13c188c6b4b095024c093bf4e91
James Phillips <scorpi4n@scorpi4n.com>
2023-03-30 13:03:55 -0500

    Style weather data output

--- src/ts/dom.ts
+++ src/ts/dom.ts
@@ -5,34 +5,40 @@ export const weatherSection: HTMLElement = document.querySelector(
   "section[aria-labelledby=city]"
 );
 export const cityEl: HTMLElement = document.querySelector("#city");
-export const lastUpdatedTimeEl: HTMLElement =
-  document.querySelector("#last-updated");
+export const lastUpdatedTimeEl: HTMLElement = document.querySelector(
+  "#last-updated .data"
+);
 export const weatherDescriptionEl: HTMLElement =
   document.querySelector("#description");
-export const temperatureEl: HTMLElement = document.querySelector("#temp");
-export const feelsLikeEl: HTMLElement = document.querySelector("#feels-like");
-export const humidityEl: HTMLElement = document.querySelector("#humidity");
-export const windSpeedEl: HTMLElement = document.querySelector("#wind-speed");
-export const cloudinessEl: HTMLElement = document.querySelector("#cloudiness");
-export const sunriseEl: HTMLElement = document.querySelector("#sunrise");
-export const sunsetEl: HTMLElement = document.querySelector("#sunset");
+export const temperatureEl: HTMLElement = document.querySelector("#temp .data");
+export const feelsLikeEl: HTMLElement =
+  document.querySelector("#feels-like .data");
+export const humidityEl: HTMLElement =
+  document.querySelector("#humidity .data");
+export const windSpeedEl: HTMLElement =
+  document.querySelector("#wind-speed .data");
+export const cloudinessEl: HTMLElement =
+  document.querySelector("#cloudiness .data");
+export const sunriseEl: HTMLElement = document.querySelector("#sunrise .data");
+export const sunsetEl: HTMLElement = document.querySelector("#sunset .data");
 
 export function displayWeather(weatherData: CurrentWeatherRes) {
   cityEl.innerText = weatherData.name;
-  lastUpdatedTimeEl.innerText =
-    "Last updated: " + new Date(weatherData.dt * 1000).toLocaleTimeString();
+  lastUpdatedTimeEl.innerText = new Date(
+    weatherData.dt * 1000
+  ).toLocaleTimeString();
   weatherDescriptionEl.innerText = toTitleCase(
     weatherData.weather[0].description
   );
-  temperatureEl.innerText = `Temp: ${weatherData.main.temp}`;
-  feelsLikeEl.innerText = `Feels like ${weatherData.main.feels_like}`;
-  humidityEl.innerText = `Humidity: ${weatherData.main.humidity}`;
-  windSpeedEl.innerText = `Wind Speed: ${weatherData.wind.speed}`;
-  cloudinessEl.innerText = `Cloudiness: ${weatherData.clouds.all}%`;
-  sunriseEl.innerText = `Sunrise: ${new Date(
+  temperatureEl.innerText = `${weatherData.main.temp}`;
+  feelsLikeEl.innerText = `${weatherData.main.feels_like}`;
+  humidityEl.innerText = `${weatherData.main.humidity}`;
+  windSpeedEl.innerText = `Wind ${weatherData.wind.speed}`;
+  cloudinessEl.innerText = `${weatherData.clouds.all}`;
+  sunriseEl.innerText = new Date(
     weatherData.sys.sunrise * 1000
-  ).toLocaleTimeString()}`;
-  sunsetEl.innerText = `Sunset: ${new Date(
+  ).toLocaleTimeString();
+  sunsetEl.innerText = new Date(
     weatherData.sys.sunset * 1000
-  ).toLocaleTimeString()}`;
+  ).toLocaleTimeString();
 }
