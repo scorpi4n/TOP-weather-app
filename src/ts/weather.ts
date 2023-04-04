b157a272b207d9cb90a80a565f077f4bd5b1ef2b
James Phillips <scorpi4n@scorpi4n.com>
2023-04-04 14:08:55 -0500

    Move list items into seperate interface

--- src/ts/weather.ts
+++ src/ts/weather.ts
@@ -67,44 +67,46 @@ export interface CurrentWeatherRes {
   cod: number;
 }
 
+export interface ForecastResItem {
+  dt: number;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    sea_level: number;
+    grnd_level: number;
+    humidity: number;
+    temp_kf: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  clouds: {
+    all: number;
+  };
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  visibility: number;
+  pop: number;
+  sys: {
+    pod: string;
+  };
+  dt_txt: string;
+}
+
 export interface ForecastRes {
   cod: number;
   message: number;
   cnt: number;
-  list: {
-    dt: number;
-    main: {
-      temp: number;
-      feels_like: number;
-      temp_min: number;
-      temp_map: number;
-      pressure: number;
-      sea_level: number;
-      grnd_level: number;
-      humidity: number;
-      temp_kf: number;
-    };
-    weather: {
-      id: number;
-      main: string;
-      description: string;
-      icon: string;
-    }[];
-    clouds: {
-      all: number;
-    };
-    wind: {
-      speed: number;
-      deg: number;
-      gust: number;
-    };
-    visibility: number;
-    pop: number;
-    sys: {
-      pod: string;
-    };
-    dt_txt: string;
-  }[];
+  list: ForecastResItem[];
   city: {
     id: number;
     name: string;
